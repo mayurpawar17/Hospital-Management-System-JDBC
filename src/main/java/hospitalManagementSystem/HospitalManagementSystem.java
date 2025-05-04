3d818baa55d12ede61360bb6115f34e850d7a918
mayurpawar17 <mayurpawar4908@gmail.com>
2025-05-04 15:24:46 +0530

    docs: In HospitalManagementSystem class  bookAppointment() & checkDoctorAvailibility() are implement

--- src/main/java/hospitalManagementSystem/HospitalManagementSystem.java
+++ src/main/java/hospitalManagementSystem/HospitalManagementSystem.java
@@ -2,6 +2,8 @@ package hospitalManagementSystem;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.util.Scanner;
 
 public class HospitalManagementSystem {
@@ -10,38 +12,47 @@ public class HospitalManagementSystem {
     private static final String password = "mayur123";
 
     public static void main(String[] args) {
-        try (Connection connection = DriverManager.getConnection(jdbcUrl, username, password); Scanner scanner = new Scanner(System.in);
+        try (Connection connection = DriverManager.getConnection(jdbcUrl, username, password);
+             Scanner scanner = new Scanner(System.in);
 
         ) {
             Patient patient = new Patient(connection, scanner);
             Doctor doctor = new Doctor(connection);
 
             while (true) {
+                System.out.println("HOSPITAL MANAGEMENT SYSTEM");
                 System.out.println("[1] Add Patient");
                 System.out.println("[2] View Patients");
-                System.out.println("[3] View Patient By ID");
-                System.out.println("[4] View Doctor");
-                System.out.println("[5] View Doctor By ID");
-                System.out.println("[6] Exit");
+                System.out.println("[3] View Doctor");
+                System.out.println("[4] Book Appointment");
+                System.out.println("[5] Exit");
+                System.out.println();
                 System.out.print("Select Option from above: ");
                 int option = scanner.nextInt();
                 switch (option) {
                     case 1:
                         patient.addPatient();
+                        System.out.println();
                         break;
                     case 2:
                         patient.viewPatient();
+                        System.out.println();
                         break;
                     case 3:
-//                        patient.getPatientById() break;
-                    case 4:
                         doctor.viewDoctor();
+                        System.out.println();
+                        break;
+                    case 4:
+                        bookAppointment(connection, scanner, patient, doctor);
+                        System.out.println();
+
                         break;
                     case 5:
-//                        doctor.getDoctorById() break;
-                    case 6:
                         System.exit(0);
                         break;
+
+                    default:
+                        System.err.println("Enter valid choice");
 
                 }
             }
@@ -50,6 +61,70 @@ public class HospitalManagementSystem {
             e.printStackTrace();
         }
     }
+
+    private static void bookAppointment(Connection connection, Scanner scanner, Patient patient, Doctor doctor) {
+        System.out.println("Enter Patient ID: ");
+        int patientId = scanner.nextInt();
+
+        System.out.println("Enter Doctor ID: ");
+        int doctorId = scanner.nextInt();
+
+        System.out.println("Enter appointment date (YYYY-MM-DD)");
+        String appointmentDate = scanner.next();
+        if (patient.getPatientById(patientId) && doctor.getDoctorById(doctorId)) {
+            if (checkDoctorAvailibity(doctorId, appointmentDate, connection)) {
+                String appointmentQuery = "insert into appointments (patient_id,doctor_id,appointment_date) values(?,?,?)";
+                try {
+                    PreparedStatement preparedStatement = connection.prepareStatement(appointmentQuery);
+                    preparedStatement.setInt(1, patientId);
+                    preparedStatement.setInt(2, doctorId);
+                    preparedStatement.setString(3, appointmentDate);
+                    int rowAffected = preparedStatement.executeUpdate();
+                    if (rowAffected > 0) {
+                        System.out.println("Appointment Booked");
+                    } else {
+                        System.out.println("Failed to book Appointment");
+                    }
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+            } else {
+                System.out.println("doctor not available on this date");
+            }
+        } else {
+            System.out.println("Either doctor or patient doesn't exit!!");
+        }
+
+
+    }
+
+    private static boolean checkDoctorAvailibity(int doctorId, String appointmentDate, Connection connection) {
+        String query = "select count(*) from appointments where doctor_id=? and appointment_date=?";
+
+        try (PreparedStatement preparedStatement = connection.prepareStatement(query);
+        ) {
+            preparedStatement.setInt(1, doctorId);
+            preparedStatement.setString(2, appointmentDate);
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+
+            if (resultSet.next()) {
+                int count = resultSet.getInt(2);
+                if (count == 0) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
 }
 
 
